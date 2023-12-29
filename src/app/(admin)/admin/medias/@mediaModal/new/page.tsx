4c44a4a624fda65bfc80c3c98d160a6adb3778c7
CLong Lam <86845927+clonglam@users.noreply.github.com>
2023-12-28 16:54:07 -0800

    Removed the File MediaForm.tsx

--- src/app/(admin)/admin/medias/@mediaModal/new/page.tsx
+++ src/app/(admin)/admin/medias/@mediaModal/new/page.tsx
@@ -1,13 +1,5 @@
 import React from "react"
 
-import SectionHeader from "@/components/SectionHeader"
-import Link from "next/link"
-import { buttonVariants } from "@/components/ui/button"
-import { db } from "@/db"
-import { notFound } from "next/navigation"
-import ProjectForm from "@/components/projects/ProjectForm"
-import MediaForm from "../../../../../../components/media/MediaForm"
-import Modal from "@/components/Modal"
 import UploadMediasModal from "@/components/media/UploadMediasModal"
 
 type Props = {}
